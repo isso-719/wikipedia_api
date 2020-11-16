bf5efc23bf1c43a8a65fdd3f2cd12e1827bfa0df
磯貝和希 <kazuki@isogaikazukinoMacBook-Pro.local>
2020-11-16 22:03:18 +0900

    near simple api

--- app.rb
+++ app.rb
@@ -7,9 +7,16 @@ require 'open-uri'
 require 'json'
 
 def url_gen(keyword)
-    base_url = 'http://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles='
-    url = base_url+keyword
+    # base_url = 'http://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles='
+    # base_url = 'http://ja.wikipedia.org/w/api.php?format=json&action=query&list=search&srlimit=3&srsearch='
+    base_url = 'https://ja.wikipedia.org/w/api.php?format=json&action=opensearch&limit=3&search='
+    url = URI.encode(base_url+keyword)
+    url = URI.parse(url)
     return url
+end
+
+get '/' do
+    'キーワードを入力してください'
 end
 
 get '/:keyword' do
@@ -17,7 +24,17 @@ get '/:keyword' do
     uri = URI(url_gen(params[:keyword]))
     json_file = open(uri).read
     hash = JSON.load(json_file)
-    @result = hash["query"]["pages"]["1024"]["extract"]
+    @results = []
+    hash[1].each do |i|
+        base_url = 'http://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles='
+        url = URI.encode(base_url+i)
+        url = URI.parse(url)
+        json_file_2 = open(url).read
+        hash_2 = JSON.load(json_file_2)
+        hash_2["query"]["pages"].each do |i|
+            @results.push(i)
+        end
+    end
 
     erb :index
 

17397e6d62fc3d00b56b744026ae5c67120c01e3
Kazuki Isogai <i@kazukiisogai.net>
2020-11-18 13:41:48 +0900

    add parameter keyword

--- app.rb
+++ app.rb
@@ -15,28 +15,32 @@ def url_gen(keyword)
     return url
 end
 
+# get '/' do
+#     "We need keyword after / .For example: https://wikipedia-api-net.herokuapp.com/Ruby"
+# end
+
 get '/' do
-    "We need keyword after / .For example: https://wikipedia-api-net.herokuapp.com/Ruby"
-end
-
-get '/:keyword' do
-    @keyword = params[:keyword]
-    uri = URI(url_gen(params[:keyword]))
-    json_file = open(uri).read
-    hash = JSON.load(json_file)
-    @results = []
-    hash[1].each do |i|
-        base_url = 'http://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles='
-        url = URI.encode(base_url+i)
-        url = URI.parse(url)
-        json_file_2 = open(url).read
-        hash_2 = JSON.load(json_file_2)
-        hash_2["query"]["pages"].each do |i|
-            @results.push(i)
+    if params['keyword']
+        keyword = params['keyword']
+        uri = URI(url_gen(keyword))
+        json_file = open(uri).read
+        hash = JSON.load(json_file)
+        @results = []
+        hash[1].each do |i|
+            base_url = 'http://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles='
+            url = URI.encode(base_url+i)
+            url = URI.parse(url)
+            json_file_2 = open(url).read
+            hash_2 = JSON.load(json_file_2)
+            hash_2["query"]["pages"].each do |i|
+                @results.push(i)
+            end
         end
+
+        erb :index
+
+    else
+        "We need keyword after / .For example: https://wikipedia-api-net.herokuapp.com/Ruby"
+
     end
-
-    erb :index
-
-
 end
